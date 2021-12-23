8bf54b9d4bfcb9775f9dee980cb439a23c09d9cd
cmorenogonzalez33 <94968881+cmorenogonzalez33@users.noreply.github.com>
2021-12-23 16:01:17 +0100

    Initial commit

---
+++ src/main/java/com/example/retoFlux/RetoFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.retoFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoFluxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/retoFlux/model/Suscriber.java
@@ -0,0 +1,10 @@
+package com.example.retoFlux.model;
+
+public class Suscriber {
+
+	public static void print(Integer n) {
+		System.out.println("Subscriber 1: " + n);
+	}
+	
+	
+}
